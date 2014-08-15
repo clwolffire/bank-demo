6a013299ae4fdbd6580e6782b41d8f7a8a3f81d2
clwolffire <clwolffire@gmail.com>
2014-08-15 02:12:55 -0500

    Adds Jacoco plug-in and a test

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -4,6 +4,16 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestBankAccount {
+public class TestBankAccount 
+
+@Test
+
+public void testDebitWithSufficientFunds () {
+
+	BankAccount account = new BankAccount(10);
+	double account = account.debit(5);
+	Assert.assertEquals(5.0, amount);
+	
+}
 
 }
